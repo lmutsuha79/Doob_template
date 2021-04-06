eb901616d24dcb43eda0693304a508287e53a081
Khelil_Yasser <lmutsuha79@gmail.com>
2021-04-06 18:36:18 +0100

    add see more and see less in galley

--- js/main.js
+++ js/main.js
@@ -79,18 +79,39 @@ let open_gallery = document.querySelector(".gallery_see");
 let gallery = document.querySelector(".portfolio_gallery");
 open_gallery.onclick = show_gallery;
 let is_gallery_open = 0;
+
 function show_gallery() {
+    let hiden_gallery_items = document.querySelectorAll(".item-hidden");
+
     if (is_gallery_open == 0) {
-        gallery.style.height = 'calc(220px * 10 + 25px * 3)';
+        hiden_gallery_items.forEach(function (item) {
+            item.style.display = 'block';
+        });
+
+
+
+
+
+
+
+
+
+
+
         open_gallery.textContent = "See less";
         is_gallery_open = 1;
     }
     else {
-        gallery.style.height = 'calc(220px * 2 + 25px*2)';
+        hiden_gallery_items.forEach(function (item) {
+            item.style.display = 'none';
+        });
+
         open_gallery.textContent = "See More";
 
         is_gallery_open = 0;
     }
+
+
 
 }
 
